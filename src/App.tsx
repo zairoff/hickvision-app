a3461db9fd514fbdd539f1c6e73e955f0c4b9ca9
zairoff <marufzoiroff@gmail.com>
2023-12-25 14:26:22 +0500

    add axios & test page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { createTheme } from "@mui/material";
+import { AppBar, createTheme } from "@mui/material";
 import SignIn from "./components/SignIn";
 import { ThemeProvider } from "@emotion/react";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
 
 function App() {
   // TODO remove, this demo shouldn't need to reset the theme.
@@ -11,7 +12,7 @@ function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <SignIn />
+      <ResponsiveAppBar />
     </ThemeProvider>
   );
 }
